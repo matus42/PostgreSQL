3c65e934e0c3aa8b69d69e61d41037acc59cd73e
matus42 <134312272+matus42@users.noreply.github.com>
2023-12-14 20:14:44 +0000

    Linking postgres and python

---
+++ sql-psycopg2.py
@@ -0,0 +1,49 @@
+import psycopg2
+
+
+# Define the connection parameters
+db_params = {
+    "database": "chinook",
+    "user": "postgres",
+    "password": "4242",
+    "host": "localhost",
+    "port": "5432"
+}
+
+
+# connect to "chinook" database
+connection = psycopg2.connect(**db_params)
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the 'Artist' table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
